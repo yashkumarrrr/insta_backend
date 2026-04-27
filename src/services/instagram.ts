87393217b12fea6e33f2826182e1a6fb24ecd429
yashkumarrrr <ashh.automation@gmail.com>
2026-04-28 00:30:51 +0530

    Update instagram.ts

--- src/services/instagram.ts
+++ src/services/instagram.ts
@@ -210,7 +210,7 @@ export class InstagramService {
       await axios.post(`${BASE_URL}/${pageId}/subscribed_apps`, null, {
         params: {
           access_token: pageToken,
-          subscribed_fields: 'messages,comments,messaging_postbacks',
+          subscribed_fields: 'messages,feed,messaging_postbacks,message_reactions',
         },
       });
       logger.info('Webhooks subscribed for page', { pageId });
